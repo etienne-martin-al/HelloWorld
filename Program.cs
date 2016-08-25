1b35e3a774b665559e7e51a77bf9843a427ce38e
ALFRGIS\Etienne.MARTIN <etienne.martin-sc@airliquide.com>
2016-08-25 18:02:36 +0200

    Init the test repository

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Etienne.MARTIN
+ * Date: 25/08/2016
+ * Time: 17:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TestCs
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("This is a test!");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
