292b15a81fe2d8e10e3d05f97cf309309becb7b5
Test Osmose <do-not-reply@osmose.airliquide.com>
2016-09-20 18:14:41 +0200

    New branch created

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace TestCs
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("This is not a test!");
+			Console.WriteLine("This is not a test or not...");
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
